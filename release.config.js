7b96eecdf3841337f9ae6886720ac7606eebdfd3
Sanjay Shahi <sanjay.shahi243@gmail.com>
2023-09-14 11:49:32 +0545

    fix: Workflow update to add assets

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ const config = {
         '@semantic-release/release-notes-generator',
         '@semantic-release/npm',
         ['@semantic-release/git', {
-            'assets': ['package.json'],
+            'assets': ['package.json', 'out/*'],
             'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }],
         '@semantic-release/github'
